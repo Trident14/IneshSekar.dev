1be296002d7f776e690719aa949732049ad06387
Inesh Sekar <80970714+Trident14@users.noreply.github.com>
2025-02-01 19:27:17 +0530

    Update vite.config.js

--- vite.config.js
+++ vite.config.js
@@ -1,14 +1,9 @@
-import { defineConfig } from 'vite';
-import react from '@vitejs/plugin-react';
-import path from 'path';
+import { defineConfig } from 'vite'
+import react from '@vitejs/plugin-react'
 
-// https://vite.dev/config/
+// https://vitejs.dev/config/
 export default defineConfig({
   plugins: [react()],
   base: '/',
-  resolve: {
-    alias: {
-      '@': path.resolve(__dirname, 'src'),  // Alias '@' to the 'src' folder
-    },
-  },
-});
+})
+
